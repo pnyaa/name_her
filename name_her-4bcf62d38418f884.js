5af18edcee3bfa2d5c33ca1209103930332662a1
pnyaa <61335367+pnyaa@users.noreply.github.com>
2026-05-16 16:39:20 +0000

    Deploying to pages from @ pnyaa/name_her@1a1e97cc19c3c7cb71a29ef0d0d757bc0467f96b 🚀

--- name_her-4bcf62d38418f884.js
+++ name_her-4bcf62d38418f884.js
@@ -454,7 +454,7 @@ function __wbg_get_imports() {
             return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 229, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 230, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h6f23c13c1dbfddd4);
             return ret;
         },
