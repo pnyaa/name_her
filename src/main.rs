3b07dc9b047b981753c8236f1c9fc9be4c6f24e9
Lawrence <61335367+pnyaa@users.noreply.github.com>
2026-05-17 14:18:13 +0100

    Use contexts to pass around Name resources

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use leptos::logging::log;
 use leptos::prelude::*;
 use leptos::{mount::mount_to_body, view, *};
 use serde::{Deserialize, Serialize};
+use std::rc::Rc;
 
 fn main() {
     mount_to_body(|| view! {<App />})
@@ -10,22 +11,16 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    // This gets compiled into the wasm binary. They are safe to redistribute
-    // but they can change over time so make them be provided as part of the env
-    log!("Getting environment");
-    let supabase_url = env!("SUPABASE_URL");
-    let publishable_api_key = env!("SUPABASE_PUBLISHABLE_API_KEY");
-    log!(
-        "Done Getting environment. Server url was \"{}\"",
-        supabase_url
-    );
+    // Provide the database details through context so they can be fetched later
+    let database = DatabaseDetails::new();
+    provide_context(database);
 
-    let suggestions = SuggestionsManager::new(supabase_url, publishable_api_key);
+    // Similarly, the list of names needs to be context fetchable, so we can
+    // display it in the list of names and also check the suggestions
+    let names_resource = LocalResource::new(move || async move { NameManager::new_async().await });
+    provide_context(NameResource(names_resource.clone()));
 
-    // Keep the list of names as a local resource that takes time to fetch
-    let names = LocalResource::new(move || async move {
-        fetch_names(&supabase_url, &publishable_api_key).await
-    });
+    let suggestions = SuggestionsManager::new();
 
     // Search bar query signals
     let (query, set_query) = signal(String::new());
@@ -38,24 +33,33 @@ fn App() -> impl IntoView {
         let q = query.get().to_lowercase();
         log!("From the filtering search bar \"{}\"", q);
 
-        match names.get() {
-            Some(names) => match names {
-                Ok(names) => Ok(names
-                    .into_iter()
-                    .filter(|n| {
-                        let rejected = match n.rejected {
-                            Some(r) => r,
-                            None => false,
-                        };
-                        if rejected && (!show_rejected.get())
-                        {
-                            return false;
-                        }
+        let name_manager = use_context::<NameResource>().expect("Database should exist");
 
-                        n.name.to_lowercase().contains(&q)
-                        })
+        match name_manager.0.get() {
+            Some(manager) => match manager.list {
+                Ok(names) => {
 
-                    .collect::<Vec<_>>()),
+                    // Extract the matching names, right now we're copying the ones that 
+                    // pass the filter so that they can be displayed. 
+                    // TODO: Try and remove the copy
+                    let v = names
+                        .iter()
+                        .filter(|n| {
+                            let rejected = match n.rejected {
+                                Some(r) => r,
+                                None => false,
+                            };
+                            if rejected && (!show_rejected.get())
+                            {
+                                return false;
+                            }
+                            n.name.to_lowercase().contains(&q)}
+                        
+                        )
+                        .cloned()
+                        .collect::<Vec<_>>();
+                    Ok(v)
+                }
                 Err(e) => Err(e),
             },
             None => Err("Failed to fetch unwrap".to_string()),
@@ -66,11 +70,11 @@ fn App() -> impl IntoView {
         <TitleEntry/>
         <div class="main">
 
-        
+
         <SleekTextInput placeholder="Search names" value=query set_value=set_query />
         <ShowRejectedBox value=show_rejected set_value=set_show_rejected />
-            
-        
+
+
 
         <NamesList names=filtered_names />
             //{AlphabetNav()}
@@ -81,6 +85,113 @@ fn App() -> impl IntoView {
 
     }
 }
+
+/// # DatabaseDetails
+/// We use this to store the public auth for the database
+#[derive(Clone, Copy)]
+struct DatabaseDetails {
+    pub url: &'static str,
+    pub api: &'static str,
+}
+
+impl DatabaseDetails {
+    fn new() -> DatabaseDetails {
+        // This gets compiled into the wasm binary. They are safe to redistribute
+        // but they can change over time so make them be provided as part of the env
+        log!("Getting environment");
+        let supabase_url = env!("SUPABASE_URL");
+        let publishable_api_key = env!("SUPABASE_PUBLISHABLE_API_KEY");
+        log!(
+            "Done Getting environment. Server url was \"{}\"",
+            supabase_url
+        );
+
+        DatabaseDetails {
+            url: supabase_url,
+            api: publishable_api_key,
+        }
+    }
+}
+
+//
+//
+//
+
+#[derive(Clone)]
+struct NameResource(LocalResource<NameManager>);
+
+#[derive(Clone)]
+struct NameManager {
+    pub list: Result<Rc<Vec<NameEntryRawDb>>, String>,
+}
+
+impl NameManager {
+    async fn new_async() -> NameManager {
+        let list = match NameEntryRawDb::get_data().await {
+            Ok(l) => Ok(Rc::new(l)),
+            Err(e) => Err(e),
+        };
+
+        NameManager { list }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct NameEntryRawDb {
+    id: i32,
+    name: String,
+    rejected: Option<bool>,
+}
+
+impl NameEntryRawDb {
+    async fn get_data() -> Result<Vec<NameEntryRawDb>, String> {
+        if cfg!(debug_assertions) {
+            get_mock_data().await
+        } else {
+            Self::get_real_data().await
+        }
+    }
+
+    pub async fn get_real_data() -> Result<Vec<NameEntryRawDb>, String> {
+        let db_details =
+            use_context::<DatabaseDetails>().expect("Failed to get the database details");
+        log!("Fetching begins");
+
+        // Address and publishable api key allowable for in browser use with rls
+        let table_url = format!("{}/rest/v1/names?select=*", db_details.url);
+
+        // Request all the names from the server
+        let resp = match Request::get(&table_url)
+            .header("apikey", db_details.api)
+            .header("Authorization", &format!("Bearer {}", db_details.api))
+            .send()
+            .await
+        {
+            Ok(resp) => resp,
+            Err(err) => {
+                log!("Got an error response {}", err);
+                return Err(format!("Failed to make request due to {}", err.to_string()));
+            }
+        };
+
+        log!("Got response {:?}", resp);
+        let names = match resp.json::<Vec<NameEntryRawDb>>().await {
+            Ok(val) => val,
+            Err(err) => {
+                let err_str = format!("Failed to deserialize due to {}", err);
+                log!("{}", err_str);
+                return Err(err_str);
+            }
+        };
+
+        log!("Fetched {} names", names.iter().len());
+        Ok(names)
+    }
+}
+
+//
+//
+//
 
 #[component]
 fn TitleEntry() -> impl IntoView {
@@ -98,7 +209,11 @@ fn TitleEntry() -> impl IntoView {
 }
 
 #[component]
-fn SleekTextInput(placeholder: &'static str, value: ReadSignal<String>, set_value: WriteSignal<String>) -> impl IntoView {
+fn SleekTextInput(
+    placeholder: &'static str,
+    value: ReadSignal<String>,
+    set_value: WriteSignal<String>,
+) -> impl IntoView {
     view! {
         <div class="search-container">
           <input
@@ -114,10 +229,10 @@ fn SleekTextInput(placeholder: &'static str, value: ReadSignal<String>, set_valu
 }
 
 #[component]
-fn ShowRejectedBox(value: ReadSignal<bool>, set_value: WriteSignal<bool>)-> impl IntoView {
+fn ShowRejectedBox(value: ReadSignal<bool>, set_value: WriteSignal<bool>) -> impl IntoView {
     view! {
         <label class="sleek-checkbox">
-            <input 
+            <input
                 type="checkbox"
                 on:change=move |e| {
                     set_value.set(event_target_checked(&e));
@@ -128,12 +243,11 @@ fn ShowRejectedBox(value: ReadSignal<bool>, set_value: WriteSignal<bool>)-> impl
         </label>
 
     }
-
 }
 
 #[component]
 fn NamesList(
-    names: impl Fn() -> Result<Vec<NameEntry>, String> + 'static + std::marker::Send,
+    names: impl Fn() -> Result<Vec<NameEntryRawDb>, String> + 'static + std::marker::Send,
 ) -> impl IntoView {
     view! {
         <div class = "scroll-viewport">
@@ -190,194 +304,7 @@ struct NameDisplayer {
     api: &'static str,
 }
 
-async fn fetch_names(url: &str, api_key: &str) -> Result<Vec<NameEntry>, String> {
-    log!("Fetching begins");
-    return Ok(vec![
-        NameEntry {
-            id: 1,
-            name: "Lacy".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "Laurel".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "Lexie".to_string(),
-            rejected: Some(true)
-        },
-        NameEntry {
-            id: 2,
-            name: "A".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "AA".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "B".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "BB".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "C".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "CC".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "D".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "DD".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "E".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "EE".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "F".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "FF".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "G".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "GG".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "H".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "HH".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "I".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "II".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "J".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "JJ".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "K".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "KK".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "L".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "LL".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "M".to_string(),
-            ..Default::default()
-        },
-        NameEntry {
-            id: 2,
-            name: "MM".to_string(),
-            ..Default::default()
-        },
-    ]);
-
-    // Address and publishable api key allowable for in browser use with rls
-    let table_url = format!("{}/rest/v1/names?select=*", url);
-
-    // Request all the names from the server
-    let resp = match Request::get(&table_url)
-        .header("apikey", api_key)
-        .header("Authorization", &format!("Bearer {}", api_key))
-        .send()
-        .await
-    {
-        Ok(resp) => resp,
-        Err(err) => {
-            log!("Got an error response {}", err);
-            return Err(format!("Failed to make request due to {}", err.to_string()));
-        }
-    };
-
-    log!("Got response {:?}", resp);
-    let names = match resp.json::<Vec<NameEntry>>().await {
-        Ok(val) => val,
-        Err(err) => {
-            let err_str = format!("Failed to deserialize due to {}", err);
-            log!("{}", err_str);
-            return Err(err_str);
-        }
-    };
-
-    log!("Fetched {} names", names.iter().len());
-    Ok(names)
-}
-
-
-
-struct SuggestionsManager
-{
-    pub url: &'static str,
-    pub api: &'static str,
-
+struct SuggestionsManager {
     pub suggestion_read: ReadSignal<String>,
     pub suggestion_write: WriteSignal<String>,
 
@@ -385,24 +312,23 @@ struct SuggestionsManager
     pub notes_write: WriteSignal<String>,
 }
 
-impl SuggestionsManager
-{
-    fn new(url: &'static str, api: &'static str) -> Self
-    {
+impl SuggestionsManager {
+    fn new() -> Self {
         let (sugg_read, sugg_write) = signal(String::new());
         let (notes_read, notes_write) = signal(String::new());
-        SuggestionsManager { url, api, suggestion_read: sugg_read, suggestion_write: sugg_write, notes_read, notes_write }
+        SuggestionsManager {
+            suggestion_read: sugg_read,
+            suggestion_write: sugg_write,
+            notes_read,
+            notes_write,
+        }
     }
-
-
 }
 
 #[component]
-fn SuggestionsRenderer(value: SuggestionsManager ) -> impl IntoView
-{
-
-    // Function to act the spawn the form submission 
-    let on_click = move|_| {
+fn SuggestionsRenderer(value: SuggestionsManager) -> impl IntoView {
+    // Function to act the spawn the form submission
+    let on_click = move |_| {
         let suggestion = value.suggestion_read.get();
         let notes = value.notes_read.get();
 
@@ -422,10 +348,10 @@ fn SuggestionsRenderer(value: SuggestionsManager ) -> impl IntoView
             }>
 
         <div class="input-group">
-        <SleekTextInput 
-            placeholder="Suggested name" 
-            value=value.suggestion_read 
-            set_value=value.suggestion_write 
+        <SleekTextInput
+            placeholder="Suggested name"
+            value=value.suggestion_read
+            set_value=value.suggestion_write
         />
 
         <button type="submit" class="sleek-button">
@@ -433,14 +359,172 @@ fn SuggestionsRenderer(value: SuggestionsManager ) -> impl IntoView
         </button>
         </div>
 
-        <SleekTextInput 
-            placeholder="Notes (From who? Why? etc.)" 
-            value=value.notes_read 
-            set_value=value.notes_write 
+        <SleekTextInput
+            placeholder="Notes (From who? Why? etc.)"
+            value=value.notes_read
+            set_value=value.notes_write
         />
 
         </form>
-        
+
     }
 }
 
+//
+//
+//
+
+/// # get_mock_data
+/// Backup provider for debug fetching of string data
+pub async fn get_mock_data() -> Result<Vec<NameEntryRawDb>, String> {
+    let _db_details = use_context::<DatabaseDetails>().expect("Failed to get the database details");
+
+    log!("Fetching begins");
+    return Ok(vec![
+        NameEntryRawDb {
+            id: 1,
+            name: "Lacy".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "Laurel".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "Lexie".to_string(),
+            rejected: Some(true),
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "A".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "AA".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "B".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "BB".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "C".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "CC".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "D".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "DD".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "E".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "EE".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "F".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "FF".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "G".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "GG".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "H".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "HH".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "I".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "II".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "J".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "JJ".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "K".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "KK".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "L".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "LL".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "M".to_string(),
+            ..Default::default()
+        },
+        NameEntryRawDb {
+            id: 2,
+            name: "MM".to_string(),
+            ..Default::default()
+        },
+    ]);
+}
